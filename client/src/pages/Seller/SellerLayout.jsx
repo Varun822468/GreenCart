c4ca76177579e4cf2b6ac35cc8133d52cbce2493
TAMILSELVAN GURUSAMY <152770808+TamilSelvanGurusamy@users.noreply.github.com>
2025-05-24 17:40:00 +0530

    seller sidebar three page created
    1.pages->seller->AddProduct.jsx file added
    2.pages->seller->Orders.jsx file added
    3.pages->seller->ProductList.jsx file added

--- client/src/pages/Seller/SellerLayout.jsx
+++ client/src/pages/Seller/SellerLayout.jsx
@@ -12,7 +12,7 @@ const SellerLayout = () => {
         { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
 
-    const logout = () => {
+    const logout = async () => {
         setIsSeller(false);
     }
 
@@ -20,7 +20,7 @@ const SellerLayout = () => {
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
                 <Link to='/'>
-                    <img className="cursor-pointer w-34 md:w-38" src={assets.logo} alt="logo" />
+                    <img src={assets.logo} alt="logo" className="cursor-pointer w-34 md:w-38" />
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
@@ -28,15 +28,10 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className="flex">
-                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
                     {sidebarLinks.map((item) => (
                         <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
-                            className={(isActive) => `flex items-center py-3 px-4 gap-3 
-                            ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary"
-                                    : "hover:bg-gray-100/90 border-white"
-                                }`
-                            }
-                        >
+                            className={(isActive) => `flex items-center py-3 px-4 gap-3 ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary" : "hover:bg-gray-100/90 border-white"}`}>
                             <img src={item.icon} alt="" className="w-7 h-7" />
                             <p className="md:block hidden text-center">{item.name}</p>
                         </NavLink>
