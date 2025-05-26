7c8390aeb391296f59c4e385e77fe4112eda9842
TAMILSELVAN GURUSAMY <152770808+TamilSelvanGurusamy@users.noreply.github.com>
2025-05-27 00:16:12 +0530

    SellerLayout screen updated

--- client/src/pages/Seller/SellerLayout.jsx
+++ client/src/pages/Seller/SellerLayout.jsx
@@ -31,7 +31,7 @@ const SellerLayout = () => {
                 <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
                     {sidebarLinks.map((item) => (
                         <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
-                            className={(isActive) => `flex items-center py-3 px-4 gap-3 ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary" : "hover:bg-gray-100/90 border-white"}`}>
+                            className={({isActive}) => `flex items-center py-3 px-4 gap-3 ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary" : "hover:bg-gray-100/90 border-white"}`}>
                             <img src={item.icon} alt="" className="w-7 h-7" />
                             <p className="md:block hidden text-center">{item.name}</p>
                         </NavLink>
