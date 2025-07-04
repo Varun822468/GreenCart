7b395ec48f0557fe1cc80c24842d9d48e626d288
TAMILSELVAN GURUSAMY <152770808+TamilSelvanGurusamy@users.noreply.github.com>
2025-07-04 22:57:50 +0530

    axios call "invalid credential" error solved
    1.client->src->App.jsx file updated
    2.clinet->src->AppContext.jsx file "fetchSeller" function added
    3.Server->controller->sellerController.jsx -> spelling mistakes correction done.

--- server/controllers/sellerContoller.js
+++ server/controllers/sellerContoller.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 // Login Seller : /api/seller/login
 export const sellerLogin = async (req, res) => {
   try {
-    const { email, passowrd } = req.body;
+    const { email, password } = req.body;
     if (
-      passowrd === process.env.SELLER_PASSWORD &&
+      password === process.env.SELLER_PASSWORD &&
       email === process.env.SELLER_EMAIL
     ) {
       const token = jwt.sign({ email }, process.env.JWT_SECRET, {
